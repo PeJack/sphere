dd0e05ad30e9ec677929e5f9b3641166e42be003
Henry Metlov <genrih.metlov@gmail.com>
2018-12-24 23:06:32 +0300

    Some fix

--- src/boilerplate/game.ts
+++ src/boilerplate/game.ts
@@ -1,19 +1,25 @@
 import "phaser";
-import { MainScene } from "./scenes/mainScene";
+import { BootScene } from "./scenes/boot-scene";
+import { MenuScene } from "../games/space-invaders/scenes/menu-scene";
+
 
 // main game configuration
 const config: GameConfig = {
+  title: "Sphere",
   width: 800,
   height: 600,
   type: Phaser.AUTO,
   parent: "game",
-  scene: MainScene,
+  scene: [BootScene, MenuScene],
   physics: {
     default: "arcade",
     arcade: {
       gravity: { y: 200 }
     }
-  }
+  },
+  backgroundColor: "#f5cc69",
+  pixelArt: true,
+
 };
 
 export class Game extends Phaser.Game {
@@ -25,3 +31,4 @@ export class Game extends Phaser.Game {
 window.onload = () => {
   var game = new Game(config);
 };
+
